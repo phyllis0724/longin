25ad214f6e9553a35ef1283731e348ba35f80e10
phyllis0724 <phyllis69347284@gmail.com>
2022-10-10 20:53:07 +0800

    option menu

--- app/src/main/java/com/example/longin/MainActivity.java
+++ app/src/main/java/com/example/longin/MainActivity.java
@@ -1,39 +1,82 @@
 package com.example.longin;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         Button btn = findViewById(R.id.login_btn) ;
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
                 EditText name = findViewById(R.id.editName);
                 EditText pw = findViewById(R.id.editPW);
                 if( ( name.getText().toString().equals("james")) &&
                         ( pw.getText().toString().equals("pw")))
                 {
+                    Bundle data = new Bundle();
+                    data.putString("account", name.getText().toString());
+                    data.putInt("age",24);
                     Intent it = new Intent(getApplicationContext(),UserActivity2.class);
-                    startActivity(it);
-                    Toast.makeText(getApplicationContext(), "登入成功",Toast.LENGTH_LONG).show();
-                }
-                else{
+                    it.putExtras(data);
+                    startActivityForResult(it,100);
+                    Toast.makeText(getApplicationContext(), "成功",Toast.LENGTH_LONG).show();
+                }else{
                     Toast.makeText(getApplicationContext(), "登入失敗",Toast.LENGTH_LONG).show();
                 }
             }
         });
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if( requestCode == 100){
+            if ( resultCode==1000) {
+                Bundle da=data.getExtras();
+                String val=da.getString("key");
+                Toast.makeText(getApplicationContext(), val, Toast.LENGTH_LONG).show();
+            }
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.mymenu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.id1:
+                new AlertDialog.Builder(this).setTitle("item1").setMessage("for test").create().show();
+
+                Toast.makeText(getApplicationContext(),"select Item1",Toast.LENGTH_LONG).show();
+                break;
+            case R.id.id2:
+                Toast.makeText(getApplicationContext(),"select Item2",Toast.LENGTH_LONG).show();
+                break;
+
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/example/longin/UserActivity2.java
+++ app/src/main/java/com/example/longin/UserActivity2.java
@@ -3,24 +3,34 @@ package com.example.longin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class UserActivity2 extends AppCompatActivity {
-
+    MediaPlayer mp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user2);
 
+        mp=MediaPlayer.create(getApplicationContext(),R.raw.dingdong);
+        mp.start();
+        Bundle data = this.getIntent().getExtras();
+        String name=data.getString("account");
+        int age = data.getInt("age");
+        Toast.makeText(getApplicationContext(),name+age, Toast.LENGTH_LONG).show();
+
         Button dialbtn = findViewById(R.id.dialbtn);
         dialbtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Uri phone = Uri.parse("tel://123456");
-                Intent it = new Intent(Intent.ACTION_DIAL,phone);
+            public void onClick(View v) {
+                Uri  phone = Uri.parse("tel://123456");
+                Intent it = new Intent(Intent.ACTION_CALL,phone);
                 startActivity(it);
             }
         });
@@ -28,19 +38,43 @@ public class UserActivity2 extends AppCompatActivity {
         Button webbtn = findViewById(R.id.webbtn);
         webbtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Uri web = Uri.parse("http://www.google.com");
-                Intent it = new Intent(Intent.ACTION_VIEW,web);
+            public void onClick(View v) {
+                Uri web = Uri.parse("http://www.pu.edu.tw");
+                Intent it  = new Intent(Intent.ACTION_VIEW, web);
                 startActivity(it);
             }
         });
-
         Button exitbtn = findViewById(R.id.exitbtn);
         exitbtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
+                Intent it =getIntent();
+                Bundle data = new Bundle();
+                data.putString("key","mydata");
+                it.putExtras(data);
+                setResult(1000,it);
                 finish();
             }
         });
+
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("log", "onStop");
+        //  mp.stop();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("log", "onPause");
+        mp.pause();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("log", "onResume");
+        mp.start();
     }
 }
